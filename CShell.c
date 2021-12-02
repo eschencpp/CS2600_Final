73b0ff170346cb68067517d0de5d7605ed6c9340
eschencpp <eschen@cpp.edu>
2021-12-02 00:56:32 +0000

    fixed errors in code

--- CShell.c
+++ CShell.c
@@ -4,32 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char **argv){
-    //Load config files, if any.
-
-    //Run command loop.
-    lsh_loop();
-
-    //Perform any shutdown /cleanup.
-
-    return EXIT_SUCCESS;
-}
-
-void lsh_loop(void){
-    char *line;
-    char **args;
-    int status;
-
-    do{
-        printf("> ");
-        line = lsh_readline();
-        args = lsh_split_line(line);
-        status = lsh_execute(args);
-
-        free(line);
-        free(args);
-    }  while (status);
-}
 
 #define LSH_RL_BUFSIZE 1024
     char *lsh_read_line(void){
@@ -62,25 +36,10 @@ void lsh_loop(void){
           buffer = realloc(buffer, bufsize);
           if (!buffer) {
               fprintf(stderr, "lsh: allocation error\n");
-              exit(EXIT_FAILURE)
+              exit(EXIT_FAILURE);
           }
       }
   }
-}
-
-char *lsh_read_line(void){
-    char *line = NULL;
-    ssize_t bufsize = 0; // have getline allocate a buffer for us
-    if (getline(&line, &bufsize, stdin) == -1){
-        if (feof(stdin)) {
-            exit(EXIT_SUCCESS);  // We recieved an EOF
-        } else  {
-            perror("readline");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    return line;
 }
 
 #define LSH_TOK_BUFSIZE 64
@@ -208,4 +167,31 @@ int lsh_execute(char **args){
         }
     }
     return lsh_launch(args);
+}
+
+void lsh_loop(void){
+    char *line;
+    char **args;
+    int status;
+
+    do{
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    }  while (status);
+}
+
+int main(int argc, char **argv){
+    //Load config files, if any.
+
+    //Run command loop.
+    lsh_loop();
+
+    //Perform any shutdown /cleanup.
+
+    return EXIT_SUCCESS;
 }
